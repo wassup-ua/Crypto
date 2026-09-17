7ec3fb9d86e80eddcb7dd593eed5f96558c83082
pimp-ua <stanislav.skruzhevskiy@gmail.com>
2026-09-17 19:23:07 +0300

    v1.1.1 Улучшение интерфейса, исправление ошибок

--- deal.py
+++ deal.py
@@ -5,23 +5,32 @@ def deal_get():
 	data_deal = input("Напиши дату сделки: ")
 	coin_name = input("Напиши название актива: ").upper()
 	while True:
-		profit_lesion = input("Ваша сделка \n[1]. Прибыльная \n[2]. Убыточная")
+		print("Ваша сделка \n[1].🟢Прибыльная \n[2].🔴Убыточная")
+		profit_lesion = input("Выбери ответ: ")
 		if profit_lesion == "1":
-			pro_les = float(input("Напиши сколько составила ваша прибыль:"))
-			plus_minus = "+"
-			break
+			while True:
+				try:
+					pro_les = float(input("🟢 Напиши сколько составила ваша прибыль: "))
+					plus_minus = "+"
+					break
+				except ValueError:
+					print("❌ Введите цифры")
 		elif profit_lesion == "2":
-			pro_les = float(input("Напиши сколько составил ваш убыток:"))
-			plus_minus = "-"
-			break
+			while True:
+				try:
+					pro_les = float(input("🔴 Напиши сколько составил ваш убыток: "))
+					plus_minus = "-"
+					break
+				except ValueError:
+					print("❌ Введите цифры")
 		else:
-			print("Такого номера нет в меню")
+			print("Такого номера нет в меню ❌")
 	comment = input("Напишите ваш комментарий по сделки: ").capitalize()
-	print("Вы добавили сделку:")
+	print("✅ Вы добавили сделку:")
 	print(f"""
 		{data_deal}
 		COIN: {coin_name}
-		{plus_minus} {pro_les} USDT
+		{plus_minus} {pro_les} USDT💲
 		Ваш комментарий: {comment}
 		""")
 
@@ -37,6 +46,7 @@ def deal_get():
 
 
 def deal_show():
+	print("📊Ваши сделки:")
 	for num, transaction in enumerate(transactions, 1):
 		print(f"{num}. {transaction['data']}. COIN: {transaction['coin']}. {transaction['p_m']} {transaction['p_l']} USDT ")
 
@@ -45,4 +55,5 @@ def deal_delete():
 	deal_show()
 	delete_num_user = int(input("Напиши номер сделки для удаления: "))
 	del transactions[delete_num_user - 1]
+	print("Вы удалили сделку ✅")
 
--- main.py
+++ main.py
@@ -10,25 +10,29 @@ while True:
 		""")
 	user_number = input("Выбери номер: ")
 	if user_number == "1":
-		print("""
-			[1]. Добавить сделку
-			[2]. Смотреть сделки
-			[3]. Удалить сделку
-			[0]. Выйти
-			""")
-		user_num1 = input("Выбери номер: ")
-		if user_num1 == "1":
-			deal.deal_get()
-		elif user_num1 == "2":
-			deal.deal_show()
-		elif user_num1 == "3":
-			deal.deal_delete()
-		elif user_num1 == "0":
-			break
-			
+		while True:
+			print("""
+				[1]. Добавить сделку
+				[2]. Смотреть сделки
+				[3]. Удалить сделку
+				[0]. Выйти
+				""")
+			user_num1 = input("Выбери номер: ")
+			if user_num1 == "1":
+				deal.deal_get()
+			elif user_num1 == "2":
+				deal.deal_show()
+			elif user_num1 == "3":
+				deal.deal_delete()
+			elif user_num1 == "0":
+				break
+			else:
+				print("Такого номера нет в меню ❌")
+
 	elif user_number  == "0":
 			break
-
+	else:
+		print("Такого номера нет в меню ❌")
 
 
 				
